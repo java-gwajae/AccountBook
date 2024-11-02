8066c4f31d5208b12229994c1fda27535737d07d
odungi <odungi04@gmail.com>
2024-11-02 15:11:35 +0900

    CREATE DB Connection

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.gwajae.accountbook {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
 
 
     opens org.gwajae.accountbook to javafx.fxml;
